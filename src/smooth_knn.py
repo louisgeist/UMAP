1c747d06821c7a1d22cfe7c21b2c1be8630e1d7f
mduv31 <mael.duvergertlse@gmail.com>
2023-12-26 21:13:41 +0100

    aucun changement smooth_knn
    simple lecture de la fonction et test sublime text

--- src/smooth_knn.py
+++ src/smooth_knn.py
@@ -1,31 +1,30 @@
 import torch
 
 
-def knn(X,k):
-	"""
-	X is a tensor of size (n,d)
-	k is the number of nearest neighbors we are looking for
+def knn(X, k):
+    """
+    X is a tensor of size (n,d)
+    k is the number of nearest neighbors we are looking for
 
-	X
+    X
 
-	"""
-	n = X.shape[0]
+    """
+    n = X.shape[0]
 
-	dots = X @ X.T
-	norms = (X**2).sum(axis = 1)
+    dots = X @ X.T
+    norms = (X**2).sum(axis=1)
 
-	dist = torch.sqrt(norms.view(n,1) + norms.view(1,n) -2 * dots)
+    dist = torch.sqrt(norms.view(n, 1) + norms.view(1, n) - 2 * dots)
 
-	knn_dists , knn = dist.topk(dim = 1, largest = False, k = k)
+    knn_dists, knn = dist.topk(dim=1, largest=False, k=k)
 
-	return knn, knn_dists
+    return knn, knn_dists
+
+    # |x-y|^2 = |x|^2 - 2<x,y> + |y|^2
+
+    # return knn, knn_dists
 
 
-	# |x-y|^2 = |x|^2 - 2<x,y> + |y|^2
-
-	#return knn, knn_dists
-
-X = torch.randn(4,2)
+X = torch.randn(10, 6)
 print(X)
-print(knn(X,2))
-
+print(knn(X, 4))
