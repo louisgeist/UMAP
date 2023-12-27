1907b49669339dc29c60e799ad16c2533c418926
mduv31 <mael.duvergertlse@gmail.com>
2023-12-27 09:24:21 +0100

    modif knn_smooth dist

--- src/smooth_knn_dist.py
+++ src/smooth_knn_dist.py
@@ -26,10 +26,6 @@ def knn(X, k):
     # return knn, knn_dists
 
 
-X = torch.randn(4, 2)
-print(X)
-print(knn(X, 2))
-
 
 def smooth_knn_dist(knn,knn_dist,err):
     
@@ -47,16 +43,29 @@ def smooth_knn_dist(knn,knn_dist,err):
     sigma_tens = torch.zeros(n)
     
     for i in range(n):
-        dist_xi = knn_dist[[i]]
-        p = dist_xi[0][1].item()
-        d1 = dist_xi[0][2].item()
-        b = (d1-p)/100
-        a = (d1-p)/math.log(n/(math.log(n)/math.log(2)))
+        dist_xi = knn_dist[[i]][0][1:]
+        p = dist_xi[0].item()
+        d_min = dist_xi[1].item()
+        d_max = dist_xi[k-2].item()
+        
+        if k>8:
+            b = d_max-p
+        else: #si k<8 on est pas assuré d'avoir une quantité positive avec d1-p
+            b = 10*(d_max-p) #en multipliant par 100 on est de nouveau sur d'avoir qqchose de >0
+            
+        a = (d_min-p)/math.log(k/(math.log(k)/math.log(2)))
+        
+        #vérif conditions initiales --> à enlever
+        test_a = torch.exp(-((dist_xi-p)/a)).sum() -math.log(k)/math.log(2)
+        test_b = torch.exp(-((dist_xi-p)/b)).sum() -math.log(k)/math.log(2)
+        if test_a>0 or test_b<0:
+            print('WARNING')
+            
         sigma = (a+b)/2
         mem = b
         while abs(mem-sigma) > err:
-            sum_exp_sigma = torch.exp(-((dist_xi-p)/sigma)).sum()
-            if sum_exp_sigma<math.log(k)/math.log(2):
+            test = torch.exp(-((dist_xi-p)/sigma)).sum() - math.log(k)/math.log(2)
+            if test<0:
                 a = sigma
             else:
                 b = sigma
@@ -65,6 +74,5 @@ def smooth_knn_dist(knn,knn_dist,err):
         sigma_tens[i] = sigma
     return sigma_tens
 
-X = torch.randn(30,10)
-res = smooth_knn_dist(knn(X, 6)[0], knn(X,6)[1],10**(-5)) 
-print(res)
+X = torch.randn(30,8)
+res = smooth_knn_dist(knn(X, 5)[0], knn(X,5)[1],10**(-5)) 
