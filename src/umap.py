81902ff20cd53c44ec9db85523f1e73417740c23
Louis Geist <louisjjgeist@gmail.com>
2024-01-08 18:32:43 +0100

    umap.py - learning time display

--- src/umap.py
+++ src/umap.py
@@ -4,20 +4,38 @@ from fuzzy_set import local_fuzzy_simplicial_set
 from embedding import spectral_embedding
 from embedding import optimize_embedding
 
+import time
+
 def UMAP(X, n_neighbors, embedding_dimension, min_dist, n_epochs):
+	t1 = time.time()
 
 	A = local_fuzzy_simplicial_set(X,n_neighbors)
+	t2 = time.time()
+
 
 	B = A + A.transpose(0,1) - A * A.transpose(0,1)
 
 	#Y = torch.randn(B.shape[0], embedding_dimension) # arbitrary initialization
 	Y = spectral_embedding(B, embedding_dimension)
+	Y_initial = Y.detach().numpy().copy() 
+	t3 = time.time()
+
 	Y = optimize_embedding(B, Y, min_dist, n_epochs)
+	t4 = time.time()
 
-	return Y.detach().numpy()
+	print("Time to compute the local fuzzy sets : ", t2-t1)
+	print("Time for computing spectral embedding : ", t3-t2)
+	print("Time for embedding optimization : ", t4-t3)
 
 
-def interlaced_ring(n_samples = 100):
+	return Y.detach().numpy() , Y_initial
+
+
+def interlaced_rings(n_samples = 100):
+	"""
+	Returns an artificial dataset composed of 2 rings that are interlaced
+	"""
+
 	data = torch.zeros(2*n_samples,3)
 
 	t = torch.linspace(0, 1, n_samples)
@@ -37,27 +55,39 @@ from mpl_toolkits.mplot3d import Axes3D
 n_samples = 100
 colors = ['blue'] * n_samples + ['red'] * n_samples
 
-X = interlaced_ring(n_samples)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+X = interlaced_rings(n_samples)
 
-x = X[:, 0].numpy()
-y = X[:, 1].numpy()
-z = X[:, 2].numpy()
+# dataset displayed in 3D
 
-ax.scatter(x, y, z, c=colors, marker='o')
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+# x = X[:, 0].numpy()
+# y = X[:, 1].numpy()
+# z = X[:, 2].numpy()
 
-plt.show()
+# ax.scatter(x, y, z, c=colors, marker='o')
 
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
 
-Y = UMAP(X, n_neighbors = 100, embedding_dimension = 2, min_dist = 0.01, n_epochs = 20)
+# plt.show()
+
+n_epochs = 400
+n_neighbors = 100
+min_dist = 0.05
+Y , Y_initial = UMAP(X, n_neighbors = n_neighbors, embedding_dimension = 2, min_dist = min_dist, n_epochs = n_epochs)
+
 
 plt.figure()
 plt.scatter(Y[:,0],Y[:,1], c = colors)
+plt.title("Reduction from 3D to 2D")
+plt.show()
+
+plt.figure()
+plt.scatter(Y_initial[:,0],Y_initial[:,1], c = colors)
+plt.title("Spectral embedding - initilialization for Y")
 plt.show()
 
 
