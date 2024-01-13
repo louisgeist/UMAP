8eb00ba13fff28423a7912c2e5408e0fe8a7ca6c
Louis Geist <louisjjgeist@gmail.com>
2024-01-13 22:14:47 +0100

    train_mnist - save of reduced data

--- src/train_mnist.py
+++ src/train_mnist.py
@@ -1,13 +1,21 @@
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib
 
 from umap import UMAP
 
-batch_size = 20
+
+batch_size = 32
+n_epoch = 400
+n_neighbors = 100
+embedding_dimension = 2
+min_dist = 0.05
+
+n_points = 15000
 
 trans2D = transforms.Compose([
         transforms.ToTensor(),
@@ -24,7 +32,6 @@ train_dataset.transform = transforms.Compose([trans2D, flatten])
 X = []
 labels = []
 
-n_points = 1000
 
 for i,x in enumerate(train_dataset) :
 	X.append(x[0])
@@ -35,9 +42,14 @@ for i,x in enumerate(train_dataset) :
 
 X = torch.stack(tuple(X))
 
+np.save(f"../data_reduced/MNIST/{n_points}pts_MNISTraw.npy",X)
+np.save(f"../data_reduced/MNIST/{n_points}pts_MNISTraw_labels.npy",labels)
 
 print("Begin")
-Y, Y_initial = UMAP(X, 60, 2, 0.05, 400)
+Y, Y_initial = UMAP(X, n_neighbors, embedding_dimension, min_dist, n_epoch)
+
+np.save(f"../data_reduced/MNIST/{n_points}pts_{n_epoch}e_{n_neighbors}knn_{min_dist}min.npy",Y)
+np.save(f"../data_reduced/MNIST/{n_points}pts_{n_epoch}e_{n_neighbors}knn_{min_dist}min_labels.npy",labels)
 
 fig, ax = plt.subplots()
 
