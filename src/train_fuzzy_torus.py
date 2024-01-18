3e2b80702552a87785c4e29d2f88cd3dbf28e942
Louis Geist <louisjjgeist@gmail.com>
2024-01-18 21:29:13 +0100

    torus fixé

--- src/train_fuzzy_torus.py
+++ src/train_fuzzy_torus.py
@@ -10,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from umap import UMAP
 
-def torus(N,r,a,R) :
+def torus(N,r,R) :
   """
   simulation of a noisy doble torus
   """
@@ -19,19 +19,20 @@ def torus(N,r,a,R) :
   simu_x = []
   simu_y = []
   simu_z = []
-  for i in range(N):
-     x = 0
-     y = 0
-     z = r+1
-    #to ensure differentiability, we use this large equation
-     while -a**2 + ((-r**2 + R**2)**2 - 2*(r**2 + R**2)*((-r - R + x)**2 + y**2) + 2*(-r**2 + R**2)*z**2 + ((-r - R + x)**2 + y**2 + z**2)**2)*((-r**2 + R**2)**2 - 2*(r**2 + R**2)*((r + R + x)**2 + y**2) + 2*(-r**2 + R**2)*z**2 + ((r + R + x)**2 + y**2 + z**2)**2)>0.01:
-        x = 8*np.random.rand()-4
-        y = 5*np.random.rand()-2.5
-        z = 2*np.random.rand()-1
-     simu_x.append(x)
-     simu_y.append(y)
-     simu_z.append(z)
 
+
+  for i in range(N):
+     U_x = 0
+     U_y = 0
+     U_z = r+1
+
+     while (R-np.sqrt(U_x**2+U_y**2))**2 + U_z**2 > r**2:
+       U_x = 8*np.random.rand()-4
+       U_y = 8*np.random.rand()-4
+       U_z = 2*np.random.rand()-1
+     simu_x.append(U_x + np.random.normal(0,0.5))
+     simu_y.append(U_y + np.random.normal(0,0.5))
+     simu_z.append(U_z + np.random.normal(0,0.5))
 
   data = torch.zeros(N,3)
   data[:,0] =  torch.tensor(simu_x)
@@ -41,12 +42,11 @@ def torus(N,r,a,R) :
   return data
 
 
-R = 1.5
-r = 0.3
-a = np.sqrt(2)
+R = 3
+r = 1
   
-N = 10**2
-data = torus(N,r,a,R)
+N = 1000
+data = torus(N,r,R)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -68,15 +68,14 @@ plt.show()
 
 
 #UMAP
-N = 10**4
-data = torus(N,r,a,R)
 n_epochs = 400
 n_neighbors = 100
 min_dist = 0.05
-Y , Y_initial = UMAP(data, n_neighbors = n_neighbors, embedding_dimension = 2, min_dist = min_dist, n_epochs = n_epochs)
+Y , Y_initial = UMAP(data, n_neighbors = n_neighbors, embedding_dimension = 2, 
+   min_dist = min_dist, n_epochs = n_epochs)
 
 
 plt.figure()
-plt.scatter(Y[:,0],Y[:,1], s = 10)
+plt.scatter(Y[:,0],Y[:,1], s = 1)
 plt.title("Reduction from 3D to 2D")
 plt.show()
